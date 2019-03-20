6fc3c68fc1f60091781c1c3e3dccae90c163789f
1442636565 <1442636565@qq.com>
2019-03-20 18:37:57 +0800

    sq

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<center>
+	<table>
+		<tr>
+			<td>用户名</td>
+			<td><input type="" name="username"></td>
+		</tr>
+		<tr>
+			<td>密码</td>
+			<td><input type="" name="password"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="" value="提交"></td>
+		</tr>
+	</table>
+</center>
+</body>
+</html>
